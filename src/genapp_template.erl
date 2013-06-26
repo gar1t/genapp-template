c04a67538c776224e2fc72065248342e34854a9f
Garrett Smith <g@rre.tt>
2013-06-26 12:25:21 -0500

    Simplify jiffy conversion + tests

--- src/genapp_template.erl
+++ src/genapp_template.erl
@@ -3,8 +3,7 @@
 
 write_template(Template, Filename) when is_list(Template) ->
     {ok, Json} = file:read_file("/tmp/metadata.json"),
-    {Vars} = jiffy:decode(Json),
-    Parsed = jiffy_to_proplist(Vars),
+    Parsed = json_to_proplist(Json),
     Content = compile_template(Template, Parsed),
     ok = file:write_file(Filename, Content).
 
@@ -14,29 +13,33 @@ compile_template(Template, Vars) when is_list(Template) ->
     {ok, Content} = Mod:render(Vars),
     Content.
 
-% The following section decodes Jiffy output to a proplist
-
-jiffy_to_proplist(JiffyList) ->
-    jiffy_to_proplist(JiffyList, []).
-jiffy_to_proplist([H|T], Proplist) ->
-    NewProplist = format_jiffy_tuplewr(H, Proplist),
-    jiffy_to_proplist(T, NewProplist);
-jiffy_to_proplist([], Proplist) ->
-    Proplist.
-
-format_jiffy_tuple(Element, Proplist) when is_tuple(Element) ->
-    {Key, Value} = Element,
-    FormattedValue = format_jiffy_tuple_value(Value),
-    lists:append([[{Key, FormattedValue}], Proplist]).
-
-format_jiffy_tuple_value(Value) when is_tuple(Value) ->
-    {JiffyList} = Value,
-    jiffy_to_proplist(JiffyList);
-format_jiffy_tuple_value(Value) ->
-    Value.
-
-% template_module returns an atom that is linked to a variable.
+json_to_proplist({L}) ->
+    json_to_proplist(L);
+json_to_proplist(L) when is_list(L) ->
+    [json_to_proplist(X) || X <- L];
+json_to_proplist({Name, Val}) ->
+    {Name, json_to_proplist(Val)};
+json_to_proplist(X) -> X.
 
 template_module(Template) ->
     Hash = erlang:phash2(Template, 10000000),
     list_to_atom("template_" ++ integer_to_list(Hash)).
+
+-include_lib("eunit/include/eunit.hrl").
+
+json_to_proplist_test() ->
+    P = fun(Str) -> json_to_proplist(jiffy:decode(Str)) end,
+    ?assertEqual(1, P("1")),
+    ?assertEqual([], P("{}")),
+    ?assertEqual([{<<"foo">>, 1}], P("{\"foo\": 1}")),
+    ?assertEqual([{<<"foo">>, <<"FOO">>}], P("{\"foo\": \"FOO\"}")),
+    ?assertEqual(
+       [{<<"foo">>, [{<<"bar">>, <<"BAR">>}]}],
+       P("{\"foo\": {\"bar\": \"BAR\"}}")),
+    ?assertEqual([], P("[]")),
+    ?assertEqual([1, 2, 3], P("[1, 2, 3]")),
+    ?assertEqual([1, <<"foo">>], P("[1, \"foo\"]")),
+    ?assertEqual(
+       [1, [{<<"foo">>, <<"FOO">>}]],
+       P("[1, {\"foo\": \"FOO\"}]")),
+    ok.
